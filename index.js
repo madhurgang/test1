f0bf6bc080e4250f7ce620fc249f957950aebd73
dimplegangwani <dimple788@gmail.com>
2019-09-12 00:13:56 +0530

    first api with fake auth

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log(' ok its working!');
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+
+})
+app.get('/a', (req, res) => res.send('Hello World!aaaa'))
+app.get('/new', (req, res) => res.send({
+  name: "mandhur mama!"
+}))
+app.post('/secured', (req, res) => {
+  const username = req.headers.username
+  const pass = req.headers.pass
+  if (username === 'manas' && pass === "asdasd")
+    res.send({
+      name: "mandhur mama is now secured!",
+      headers: req.headers
+    })
+  else
+    res.status(401).send({
+      err: 'unauthoriseed'
+    })
+})
+
+app.listen(port, () => console.log(`url is http://localhost:${port}!`))
